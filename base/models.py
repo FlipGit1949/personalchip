df54a5987fed39408ba544363e201a9f6c6defc0
Felix Beekman <fbeekman@upcmail.nl>
2021-10-19 19:10:16 +0200

    ckeditor removed

--- base/models.py
+++ base/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 import uuid
 from validators import file_size
-from ckeditor_uploader.fields import RichTextUploadingField
 
 class Project(models.Model):
 	title = models.CharField(max_length=200)
 	thumbnail = models.ImageField(null=True)
-	body = RichTextUploadingField()
+	body = models.CharField(max_length=200, null=True)
 	slug = models.SlugField(null=True, blank=True)
 	created = models.DateTimeField(auto_now_add=True)
 	id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
