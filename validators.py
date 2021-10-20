d51908c617e09b80e07141d565dccb5a2f689c19
Felix Beekman <fbeekman@upcmail.nl>
2021-10-20 16:13:28 +0200

    validator

--- validators.py
+++ validators.py
@@ -2,5 +2,5 @@ from django.core.exceptions import ValidationError
 
 def file_size(value):
 	filesize = value.size
-	if filesize > 1258291200:
-		raise ValidationError('maximum size is 150 mb')
+	if filesize > 2097152000:
+		raise ValidationError('maximum size is 250 mb')
